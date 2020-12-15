ad3c68d0e42a9ddbcacdbf4b9a52443ffcf77722
Bruno Lima <brunolima.cad@gmail.com>
2020-12-15 00:05:42 -0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import MenuBar from './components/menu-bar/MenuBar';
+import HomeView from './components/home-view/HomeView';
+import StartScreen from './components/start-screen/StartScreen'
+
+interface Context {
+  mainSubjects: string[],
+  setMainSubjects: React.Dispatch<React.SetStateAction<string[]>>
+}
+const mainSubjects: string[] = [];
+const setMainSubjects: React.Dispatch<React.SetStateAction<string[]>> = () => { }
+export const appContext = createContext<Context>(
+  {
+    mainSubjects,
+    setMainSubjects
+  }
+);
 
 export default function App() {
+
+  const [isStarting, setIsStarting] = useState<boolean>(true)
+  const [mainSubjects, setMainSubjects] = useState<string[]>([
+    'Theology',
+    'Economics'
+  ])
+
+  useEffect(() => {
+    if (isStarting) {
+      setTimeout(() => {
+        setIsStarting(false);
+      }, 5000);
+    }
+  }, [isStarting]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <appContext.Provider value={
+        {
+          mainSubjects,
+          setMainSubjects
+        }}>
+
+        {
+          isStarting && <StartScreen />
+        }
+
+        {
+          !isStarting && <MenuBar />
+        }
+
+        {
+          !isStarting && <HomeView />
+        }
+
+      </appContext.Provider>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexBasis: '100%',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
